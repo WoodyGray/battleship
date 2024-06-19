606829246fde06e1fd54f22ebf1488be7edf6b47
WoodyGray <115577474+WoodyGray@users.noreply.github.com>
2024-06-19 06:51:23 +0300

    maven quickstart
    init project and dependencies

---
+++ battleship/src/main/java/org/woody/App.java
@@ -0,0 +1,13 @@
+package org.woody;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
